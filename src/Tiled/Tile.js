e81960045b5002a3dc1784e2d4394e95d4661d66
Reynau <xavi.lopez.reynau@gmail.com>
2017-04-05 17:30:31 +0200

    Add Tiled files. Ref: https://github.com/riebel/pixi-tiledmap

---
+++ src/Tiled/Tile.js
@@ -0,0 +1,56 @@
+function Tile(tile, tileSet, horizontalFlip, verticalFlip, diagonalFlip) {
+    var textures = [];
+
+    if (tile.animations.length) {
+        tile.animations.forEach(function(frame) {
+            textures.push(tileSet.textures[frame.tileId]);
+        }, this);
+    }
+    else {
+        textures.push(tileSet.textures[tile.gid - tileSet.firstGid]);
+    }
+
+    PIXI.extras.MovieClip.call(this, textures);
+
+    for (var property in tile) {
+        if (tile.hasOwnProperty(property)) {
+            this[property] = tile[property];
+        }
+    }
+
+    if (horizontalFlip) {
+        this.anchor.x = 1;
+        this.scale.x = -1;
+    }
+
+    if (verticalFlip) {
+        this.anchor.y = 1;
+        this.scale.y = -1;
+    }
+
+    if (diagonalFlip) {
+        if (horizontalFlip) {
+            this.anchor.x = 0;
+            this.scale.x = 1;
+            this.anchor.y = 1;
+            this.scale.y = 1;
+
+            this.rotation = PIXI.DEG_TO_RAD * 90;
+        }
+        if (verticalFlip) {
+            this.anchor.x = 1;
+            this.scale.x = 1;
+            this.anchor.y = 0;
+            this.scale.y = 1;
+
+            this.rotation = PIXI.DEG_TO_RAD * -90;
+        }
+    }
+
+    this.textures = textures;
+    this.tileSet = tileSet;
+}
+
+Tile.prototype = Object.create(PIXI.extras.MovieClip.prototype);
+
+module.exports = Tile;
---
+++ src/Tiled/TiledMap.js
@@ -0,0 +1,55 @@
+var TileSet = require('./Tileset'),
+    TileLayer = require('./TileLayer'),
+    ImageLayer = require('./ImageLayer'),
+    path = require('path');
+
+function TiledMap(resourceUrl) {
+    PIXI.Container.call(this);
+
+    var route = path.dirname(resourceUrl);
+    var data = PIXI.loader.resources[resourceUrl].data;
+
+    for (var property in data) {
+        if (data.hasOwnProperty(property)) {
+            this[property] = data[property];
+        }
+    }
+
+    this.tileSets = [];
+    this.layers = [];
+
+    this.background = new PIXI.Graphics();
+    this.background.beginFill(0x000000, 0);
+    this.background.drawRect(0, 0, this._width * this.tileWidth, this._height * this.tileHeight);
+    this.background.endFill();
+    this.addLayer(this.background);
+
+    data.tileSets.forEach(function(tilesetData) {
+        this.tileSets.push(new TileSet(route, tilesetData));
+    }, this);
+
+    data.layers.forEach(function(layerData) {
+        switch (layerData.type) {
+            case 'tile':
+                var tileLayer = new TileLayer(layerData, this.tileSets);
+                this.layers[layerData.name] = tileLayer;
+                this.addLayer(tileLayer);
+                break;
+            case 'image':
+                var imageLayer = new ImageLayer(layerData, route);
+                this.layers[layerData.name] = imageLayer;
+                this.addLayer(imageLayer);
+                break;
+            default:
+                this.layers[layerData.name] = layerData;
+        }
+    }, this);
+}
+
+TiledMap.prototype = Object.create(PIXI.Container.prototype);
+
+TiledMap.prototype.addLayer = function(layer) {
+    this.addChild(layer);
+};
+
+module.exports = TiledMap;
